44b042443815705aa17f5bd36523736e7d199ae9
Ximena-Matias-Cari <24680159@cuautla.tecnm.mx>
2026-02-08 21:16:46 -0600

    Create cal.py

---
+++ cal.py
@@ -0,0 +1,67 @@
+import flet as ft
+
+def main(page: ft.Page):
+
+    page.title = "Calculadora TAP"
+    page.window_width = 250
+    page.window_height = 400
+    page.padding = 20
+
+    # DISPLAY
+    display = ft.Container(
+        content=ft.Text("0", size=30),
+        bgcolor=ft.Colors.BLACK12,
+        border_radius=8,
+        alignment=ft.alignment.Alignment(1, 0),
+        padding=10,
+        width=210,
+        height=70,
+    )
+
+    # FUNCION DEL CLICK
+    def agregar_numero(e):
+        valor = e.control.data
+
+        if display.content.value == "0":
+            display.content.value = valor
+        else:
+            display.content.value += valor
+
+        page.update()
+
+    # GRID DE BOTONES
+    grid = ft.GridView(
+        runs_count=3,
+        spacing=10,
+        run_spacing=10,
+        width=210,
+        height=220,
+        expand=False
+    )
+
+    # BOTONES NUMERICOS
+    numeros = ["7","8","9","4","5","6","1","2","3","0"]
+
+    for n in numeros:
+        grid.controls.append(
+            ft.ElevatedButton(
+                text=n,
+                data=n,
+                on_click=agregar_numero,
+                height=50
+            )
+        )
+
+    # LAYOUT PRINCIPAL
+    layout_principal = ft.Column(
+        controls=[
+            display,
+            grid
+        ],
+        tight=True
+    )
+
+    page.add(layout_principal)
+    page.update()
+
+ft.app(target=main)
